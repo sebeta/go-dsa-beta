7a5fd0342d480beb55be3e3fdac0eda34df57ad4
Sebastián Betancur <sebeta057@gmail.com>
2026-08-21 10:30:15 -0500

    feat: add my own solution and comments for BubbleSort exercise

--- array/bubble_sort.go
+++ array/bubble_sort.go
@@ -1,6 +1,39 @@
 package array
 
 // BubbleSort solves the problem in O(n^2) time and O(1) space.
-// TODO: implement
+// wasSwapped is used to track if any swaps occurred in the current pass,
+// so we can break early if no swaps were made (i.e., the array is sorted).
+//
+// This was my solution.
 func BubbleSort(input []int) {
+	n := len(input)
+	for i := 0; i < n; i++ {
+		wasSwapped := false
+		for j := 0; j < n-i-1; j++ {
+			if input[j] > input[j+1] {
+				input[j], input[j+1] = input[j+1], input[j]
+				wasSwapped = true
+			}
+		}
+
+		if !wasSwapped {
+			break
+		}
+	}
+}
+
+// BubbleSort2 solves the problem in O(n^2) time and O(1) space.
+//
+// This is the solution from the original repo.
+func BubbleSort2(input []int) {
+	swapped := true
+	for swapped {
+		swapped = false
+		for i := 1; i < len(input); i++ {
+			if input[i] < input[i-1] {
+				input[i], input[i-1] = input[i-1], input[i]
+				swapped = true
+			}
+		}
+	}
 }
